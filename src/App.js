aed2bb891fd256440d96def362a514727848b678
daseulkim128 <70882276+daseulkim128@users.noreply.github.com>
2022-06-14 23:53:31 +0900

    food 예제 recap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import PropTypes from "prop-types";
+
+function Food({name, image}){
+ return (
+          <div>
+              <h1>i love {name}</h1>
+              <img src={image} alt={name}/>   
+          </div>
+        )
+}
+
+const foods = [
+  { id : 1, name : "kimchi1", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"},
+  { id : 2, name : "kimchi2", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"},
+  { id : 3, name : "kimchi3", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"}
+];
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  image : PropTypes.string.isRequired
+}
+
+
 function App() {
   return (
     <div className="App">
-      git branch test
+      {foods.map(food=>(
+        <Food key={food.id} name={food.name} image={food.image}/>
+      ))}
     </div>
   );
 }
