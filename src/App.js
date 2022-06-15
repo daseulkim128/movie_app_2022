16003b04136fc8339486eed426e7b80d1df9ed4c
daseulkim128 <70882276+daseulkim128@users.noreply.github.com>
2022-06-16 07:27:46 +0900

    app class

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import PropTypes from "prop-types";
 
-function Food({name, image}){
- return (
-          <div>
-              <h1>i love {name}</h1>
-              <img src={image} alt={name}/>   
-          </div>
-        )
+import React from "react";
+
+class App extends React.Component{
+  
+  state = {
+    age : 10
+  }
+  
+  add=()=>{
+    this.setState(current => ({age : current.age+1}))
+  }
+
+  minus=()=>{
+    this.setState(current => ({age : current.age-1}))
+  }
+  
+  render(){
+    return (
+              <div>
+                <h1>age : {this.state.age}</h1>
+                <button onClick={this.add}>add</button>
+                <button onClick={this.minus}>minus</button>
+              </div>
+            )
+  }
 }
-
-const foods = [
-  { id : 1, name : "kimchi1", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"},
-  { id : 2, name : "kimchi2", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"},
-  { id : 3, name : "kimchi3", image : "https://mykoreankitchen.com/wp-content/uploads/2022/01/5.-Homemade-Kimchi.jpg"}
-];
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  image : PropTypes.string.isRequired
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      {foods.map(food=>(
-        <Food key={food.id} name={food.name} image={food.image}/>
-      ))}
-    </div>
-  );
-}
-
 export default App;
